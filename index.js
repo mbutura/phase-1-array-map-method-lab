59c9d5b796dab23c7b86454abb80e4aeacfbb260
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2022-12-04 16:58:15 +0000

    Complete assignment

--- index.js
+++ index.js
@@ -11,6 +11,4 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(title => title.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '));
